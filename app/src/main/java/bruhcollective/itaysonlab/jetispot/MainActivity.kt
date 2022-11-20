de36ae9a0734e3137f5c44777f38ee76ed4bfe68
DotsTeaLab <dotstealab@gmail.com>
2022-11-20 05:47:11 +0100

    Now playing bottom sheet: blur and scale calculations do not take place in animate*AsState() anymore so theres now no delay between opening the bottom sheet and background visual effects making it feel more responsive. Scale and blur effects are now passed through cubic bezier easing(scale effect diminishes when the player is half open. and i find it pretty).

--- app/src/main/java/bruhcollective/itaysonlab/jetispot/MainActivity.kt
+++ app/src/main/java/bruhcollective/itaysonlab/jetispot/MainActivity.kt
@@ -8,9 +8,8 @@ import androidx.activity.addCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -152,56 +151,56 @@ class MainActivity : ComponentActivity() {
         CompositionLocalProvider(LocalNavigationController provides lambdaNavController) {
           ModalBottomSheetLayout(
             bottomSheetNavigator = bottomSheetNavigator,
-            sheetShape = RoundedCornerShape(topStart = 28.dp,topEnd = 28.dp),
+            sheetShape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp),
             scrimColor = MaterialTheme.colorScheme.scrim.copy(0.5f),
             sheetBackgroundColor = MaterialTheme.colorScheme.surface
           ) {
-             Scaffold(
-               bottomBar = {
-                 val currentDestination = navBackStackEntry?.destination
-                 if (Screen.hideNavigationBar.any { it == currentDestination?.route }) return@Scaffold
-                 NavigationBar(
-                   modifier = Modifier
-                     .offset {
-                       IntOffset(
-                         0,
-                         ((80.dp + navBarHeightDp).toPx() * bsOffset() * 3f).toInt()
-                       )
-                     }
-                     .background(MaterialTheme.colorScheme.compositeSurfaceElevation(3.dp))
-                 ) {
-                   Screen.showInBottomNavigation.forEach { (screen, icon) ->
-                     NavigationBarItem(
-                       icon = {
-                         Icon(
-                           icon,
-                           contentDescription = stringResource(screen.title)
-                         )
-                              },
-                       label = { Text(stringResource(screen.title)) },
-                       selected = lambdaNavController.controller().backQueue.any {
-                         it.destination.route?.startsWith(
-                           screen.route
-                         ) == true },
-                       onClick = {
-                         navController.navigate(screen.route) {
-                           popUpTo(Screen.NavGraph.route) {
-                             saveState = true
-                           }
-                           launchSingleTop = true
-                           restoreState = true
-                         }
-                       }
-                     )
-                   }
-                 }
-               }
+            Scaffold(
+              bottomBar = {
+                val currentDestination = navBackStackEntry?.destination
+                if (Screen.hideNavigationBar.any { it == currentDestination?.route }) return@Scaffold
+                NavigationBar(
+                  modifier = Modifier
+                    .offset {
+                      IntOffset(
+                        0,
+                        ((80.dp + navBarHeightDp).toPx() * bsOffset() * 3f).toInt()
+                      )
+                    }
+                    .background(MaterialTheme.colorScheme.compositeSurfaceElevation(3.dp))
+                ) {
+                  Screen.showInBottomNavigation.forEach { (screen, icon) ->
+                    NavigationBarItem(
+                      icon = {
+                        Icon(
+                          icon,
+                          contentDescription = stringResource(screen.title)
+                        )
+                             },
+                      label = { Text(stringResource(screen.title)) },
+                      selected = lambdaNavController.controller().backQueue.any {
+                        it.destination.route?.startsWith(
+                          screen.route
+                        ) == true },
+                      onClick = {
+                        navController.navigate(screen.route) {
+                          popUpTo(Screen.NavGraph.route) {
+                            saveState = true
+                          }
+                          launchSingleTop = true
+                          restoreState = true
+                        }
+                      }
+                    )
+                  }
+                }
+              }
             ) { innerPadding ->
-               val sheetPeekHeight by animateDpAsState(
-                 if (bsVisible) 80.dp + 64.dp + navBarHeightDp else 0.dp
-               )
+              val sheetPeekHeight by animateDpAsState(
+                if (bsVisible) 80.dp + 64.dp + navBarHeightDp else 0.dp
+              )
 
-               BottomSheetScaffold(
+              BottomSheetScaffold(
                 sheetContent = {
                   NowPlayingScreen(
                     bottomSheetState = bsState.bottomSheetState,
@@ -219,8 +218,20 @@ class MainActivity : ComponentActivity() {
                 backgroundColor = MaterialTheme.colorScheme.surface,
                 sheetGesturesEnabled = !bsQueueOpened
               ) { innerScaffoldPadding ->
-                val zoom by animateFloatAsState(1f - bsOffset() * 0.1f, spring())
-                val blur by animateDpAsState(64.dp * bsOffset(), spring())
+                val cbc1 = 0.0f
+                val cbc2 = 1.36f
+                val cbc3 = 0.13f
+                val cbc4 = 0.75f
+
+                val cubicEasing = remember { CubicBezierEasing(cbc1, cbc2, cbc3, cbc4) }
+
+                val scale = remember {
+                  derivedStateOf {
+                    cubicEasing.transform(1f - bsOffset() * 0.8f)
+                  }
+                }
+
+                val blur = remember { derivedStateOf { (cubicEasing.transform(bsOffset() * 32)).dp } }
 
                 AppNavigation(
                   navController = navController,
@@ -228,10 +239,10 @@ class MainActivity : ComponentActivity() {
                   authManager = authManager,
                   bsVisible = bsVisible,
                   modifier = Modifier
-                    .blur(blur)
+                    .blur(blur.value)
                     .graphicsLayer {
-                      scaleX = zoom
-                      scaleY = zoom
+                      scaleX = scale.value
+                      scaleY = scale.value
                     }
                     .padding(innerScaffoldPadding)
                     .padding(bottom = if (bsVisible) 0.dp else 80.dp)
